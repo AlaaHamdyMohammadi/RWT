508961e9d9a0732a8858ebae023df1c0db1b0eba
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-19 13:58:17 +0200

    Update

--- Section26/Features/arrays.ts
+++ Section26/Features/arrays.ts
@@ -1,4 +1,8 @@
-const carMakers = ['ford', 'toyota', 'chevy'];
+const carMakers: (string | number)[] = ['ford', 'toyota', 'chevy', 10];
 
 // Help with inference when extracting values
 const myCar = carMakers.pop();
+const valid = carMakers.push(10)
+
+// Flexible types
+const importantDates = []
