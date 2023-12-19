0b4f66c19f3e97f8660109a8df4ece819c29a1ac
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-19 03:18:09 +0200

    Update

--- Section26/Features/annotations/functions.ts
+++ Section26/Features/annotations/functions.ts
@@ -10,3 +10,9 @@ function divide(a: number, b: number): number{
 const muliply = function(a: number, b: number){
     return a * b;
 }
+
+// Destructure
+
+const logWeather = ({date, weather}: {date: Date; weather: string}): void => {
+    console.log(date, weather);
+}
