9641bef82e0e7a2d521e5b4b041871171246f05e
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-19 02:03:03 +0200

    Update

---
+++ Section26/Features/annotations/functions.ts
@@ -0,0 +1,12 @@
+const add = (a: number, b: number) : number => {
+    return a + b;
+}
+
+function divide(a: number, b: number): number{
+    return a / b;
+}
+
+// Anonymous functions
+const muliply = function(a: number, b: number){
+    return a * b;
+}
--- Section26/Features/annotations/variables.ts
+++ Section26/Features/annotations/variables.ts
@@ -18,3 +18,19 @@ let point: { x: number; y: number} = {
 const logNumber: (i: number) => void = (i: number) =>{
     console.log(i);
 }
+
+// Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: {x: number, y: number} = JSON.parse(json) // convert string into object
+console.log(coordinates);
+
+// variable whose type cannot be inferred correctly
+
+let numbers = [-10, 20, 0];
+let numberAboveZero: boolean | number = false;
+
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] > 0){
+        numberAboveZero = numbers[i];
+    }
+}
